0c2f0905d938ca55c939bb1774d4b736ab34ad7a
Gulnaz A. <akhmetovagrm@gmail.com>
2023-10-23 15:04:09 +0500

    Corrected main.py

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import requests
 from tqdm import tqdm
 from datetime import datetime
 
-# pip freeze > requirements.txt
-TOKEN_VK = ''
 
-class VK_get_photos:
+class VkPhotoGetter:
     def __init__(self, token, version='5.131'):
         self.token = token
         self.version = version
         self.params = {'access_token': self.token, 'v': self.version}
+        
 
-    def get_photos(self):
+    def get_photos(self, user_id):
+        self.user_id = user_id
         url_get_photos = 'https://api.vk.com/method/photos.get'
         params = {'owner_id': user_id,
                       'album_id': 'profile',
@@ -26,7 +26,7 @@ class VK_get_photos:
         return responce
 
     def best_size_photos(self):
-        data = self.get_photos()
+        data = self.get_photos(self.user_id)
         photo_dict =  {}
         photo_json = {}
         photo_json_1 = {}
@@ -53,14 +53,15 @@ class VK_get_photos:
                 
         return photo_dict             
                  
-class YADISK:
+class YandexDiskLoading:
     def __init__(self, token):
         self.token = token
         self.headers = {'Content-Type': 'application/json',
                        'Authorization': self.token}
 
 # Создание папки на Яндекс.Диск
-    def put_folder(self):
+    def put_folder(self, folder_name):
+        self.folder_name = folder_name
         url_folder = 'https://cloud-api.yandex.net/v1/disk/resources/'       
         params = {'path': folder_name}
         responce = requests.put(url_folder, headers=self.headers, params=params) 
@@ -73,25 +74,23 @@ class YADISK:
     def download_photo(self):
         for photo in tqdm(url_upload.items()):
             url_download = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-            params = {'path': f'{folder_name}/{photo[0]}', 'url': {photo[1]}}  
+            params = {'path': f'{self.folder_name}/{photo[0]}', 'url': {photo[1]}}  
             res = requests.post(url_download, headers=self.headers, params=params) 
             
         if 200 <= res.status_code <=300:
-            return f'Файлы загружены в папку {folder_name}'
+            return f'Файлы загружены в папку {self.folder_name}'
         else:
             return f'Ошибка, {res.status_code}' 
 
 
 
-user_id = input('Введите id пользователя VK для скачиваний фотографий профиля: ')    
-vk_client = VK_get_photos(TOKEN_VK)
-vk_client.get_photos() 
+vk_client = VkPhotoGetter(input('Введите свой токен VK: '))
+vk_client.get_photos(input('Введите id пользователя VK для скачиваний фотографий профиля: ')) 
 url_upload = vk_client.best_size_photos()
 
-folder_YA = YADISK(input("Введите свой токен для Яндекс.Диска: "))
-folder_name = input('Введите название папки для сохранения фотографий в Яндекс.Диске: ')
+folder_YA = YandexDiskLoading(input("Введите свой токен для Яндекс.Диска: "))
 
-pprint(folder_YA.put_folder())
-pprint(folder_YA.download_photo())
+print(folder_YA.put_folder(input("Введите название папки: ")))
+print(folder_YA.download_photo())
 
 
